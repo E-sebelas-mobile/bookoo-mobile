35f5744f6d6961469cbf0efcd4c6f0e07471d3ac
Gyan Bano <gyanamurtiadhikarabano@gmail.com>
2023-12-11 20:22:23 +0700

    base report

--- lib/reportbookstuff/report.dart
+++ lib/reportbookstuff/report.dart
@@ -1,35 +1,35 @@
 // To parse this JSON data, do
 //
-//     final report = reportFromJson(jsonString);
+//     final product = productFromJson(jsonString);
 
 import 'dart:convert';
 
-List<Product> reportFromJson(String str) => List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+List<Product> productFromJson(String str) => List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
 
-String reportToJson(List<Product> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String productToJson(List<Product> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Product {
-  String model;
-  int pk;
-  Fields fields;
+    String model;
+    int pk;
+    Fields fields;
 
-  Product({
-    required this.model,
-    required this.pk,
-    required this.fields,
-  });
+    Product({
+        required this.model,
+        required this.pk,
+        required this.fields,
+    });
 
-  factory Product.fromJson(Map<String, dynamic> json) => Product(
-        model: json["model"] ?? 'Product',
-        pk: json["id"],
+    factory Product.fromJson(Map<String, dynamic> json) => Product(
+        model: json["model"],
+        pk: json["pk"],
         fields: Fields.fromJson(json["fields"]),
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "model": model,
         "pk": pk,
         "fields": fields.toJson(),
-      };
+    };
 }
 
 class Fields {
@@ -39,7 +39,7 @@ class Fields {
     String description;
     int user;
     String status;
-    String dateAdded;
+    DateTime dateAdded;
 
     Fields({
         required this.bookTitle,
@@ -58,7 +58,7 @@ class Fields {
         description: json["description"],
         user: json["user"],
         status: json["status"],
-        dateAdded: json["date_added"],
+        dateAdded: DateTime.parse(json["date_added"]),
     );
 
     Map<String, dynamic> toJson() => {
@@ -68,6 +68,6 @@ class Fields {
         "description": description,
         "user": user,
         "status": status,
-        "date_added": dateAdded,
+        "date_added": "${dateAdded.year.toString().padLeft(4, '0')}-${dateAdded.month.toString().padLeft(2, '0')}-${dateAdded.day.toString().padLeft(2, '0')}",
     };
 }
